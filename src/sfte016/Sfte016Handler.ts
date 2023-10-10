d5d5baed5d46b088faa9de82cade4e3d48c99597
tassun_oro <tassun_oro@hotmail.com>
2023-10-10 12:36:07 +0700

    alter table and call mail service

--- src/sfte016/Sfte016Handler.ts
+++ src/sfte016/Sfte016Handler.ts
@@ -1,9 +1,10 @@
+import { v4 as uuid } from 'uuid';
 import { KnModel, KnOperation } from "@willsofts/will-db";
 import { KnDBConnector, KnSQLInterface, KnRecordSet, KnSQL, KnResultSet } from "@willsofts/will-sql";
 import { HTTP } from "@willsofts/will-api";
 import { KnUtility, KnPageUtility, VerifyError, KnValidateInfo, KnContextInfo, KnNotifyConfig, KnDataTable, KnTemplateInfo } from '@willsofts/will-core';
 import { Utilities } from "@willsofts/will-util";
-import { PasswordLibrary, MailLibrary, MailInfo } from "@willsofts/will-lib";
+import { PasswordLibrary } from "@willsofts/will-lib";
 import { TknAccountHandler } from "@willsofts/will-serv";
 import { OPERATE_HANDLERS } from "@willsofts/will-serv";
 import { DEFAULT_PRIVILEGES } from "../utils/EnvironmentVariable";
@@ -271,13 +272,15 @@ export class Sfte016Handler extends TknOperateHandler {
             found = rs.rows.length>0;
             if(found) return Promise.reject(new VerifyError("Email is already existed",HTTP.NOT_ACCEPTABLE,-18879));
             found = false;
+            /*
             knsql.clear();
             knsql.append("select userid ");
             knsql.append("from tuserinfo ");
             knsql.append("where userid = ?userid ");
-            knsql.set("userid",context.params.userid);
+            knsql.set("userid",context.params.username);
             rs = await knsql.executeQuery(db,context);
             found = rs.rows.length>0;
+            */
             //rs = await this.createUserTableToConfirm(context, model, db, found, eng);
             rs = await this.createUserTableToActivate(context, model, db, found, eng);
             return rs;
@@ -367,7 +370,7 @@ export class Sfte016Handler extends TknOperateHandler {
         let site = context.params.site;
         if(!site || site.trim()=="") site = this.userToken?.site;
         let curdate = Utilities.now();
-        context.params.userid = context.params.username;
+        context.params.userid = uuid();
         let plib = new PasswordLibrary();
         let passwordexpiredate = await plib.getUserExpireDate(db, context.params.userid, curdate);
         let userpassword = context.params.userpassword;
@@ -389,7 +392,7 @@ export class Sfte016Handler extends TknOperateHandler {
                 knsql.append("insert into tuserinfo(site,employeeid,userid,userename,useresurname,usertname,usertsurname,email,gender,mobile,lineid,inactive,editdate,edittime,edituser) ");
                 knsql.append("values(?site,?employeeid,?userid,?userename,?useresurname,?usertname,?usertsurname,?email,?gender,?mobile,?lineid,?inactive,?editdate,?edittime,?edituser) ");
                 knsql.set("site",site);
-                knsql.set("employeeid",context.params.userid);
+                knsql.set("employeeid",context.params.username);
                 knsql.set("userid",context.params.userid);
                 knsql.set("usertname",context.params.usertname);
                 knsql.set("usertsurname",context.params.usertsurname);
