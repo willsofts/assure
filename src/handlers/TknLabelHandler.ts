6dea7ad85403641037c2eaa513787bb5fa129d2a
tassun <32536248+tassun@users.noreply.github.com>
2024-09-17 18:21:43 +0700

    Update TknLabelHandler.ts

--- src/handlers/TknLabelHandler.ts
+++ src/handlers/TknLabelHandler.ts
@@ -1,10 +1,11 @@
 import { KnModel } from "@willsofts/will-db";
+import { KnRecordSet, KnResultSet } from "@willsofts/will-sql";
 import { HTTP } from "@willsofts/will-api";
 import { Utilities } from "@willsofts/will-util";
 import { TknSchemeHandler } from "./TknSchemeHandler";
 import { VerifyError } from "../models/VerifyError";
 import { KnLabelUtility } from "../utils/KnLabelUtility";
-import { KnContextInfo, KnValidateInfo, KnLabelData, KnDataEntity } from '../models/KnCoreAlias';
+import { KnContextInfo, KnValidateInfo, KnLabelData, KnDataEntity, KnDataSet } from '../models/KnCoreAlias';
 
 export class TknLabelHandler extends TknSchemeHandler {
     public model : KnModel = { name: "tlabel", alias: { privateAlias: this.section } };
@@ -34,6 +35,51 @@ export class TknLabelHandler extends TknSchemeHandler {
         let util = new KnLabelUtility(context.params.labelname);
         let data = await util.load(workdir);
         return this.createCipherData(context, "list", data);
+    }
+
+    protected buildLabelData(rs: KnResultSet) : Map<string,KnLabelData> {
+        let maps = new Map<string,KnLabelData>();
+        if(rs && rs.rows?.length > 0) {
+            for(let row of rs.rows) {
+                let lang = row.langcode;
+                let name = row.labelname;
+                let value = row.labelvalue;
+                let data = maps.get(lang);
+                if(!data) {
+                    data = { language: lang, label: [] };
+                    maps.set(lang,data);
+                }
+                data.label.push({name: name, value: value});
+            }
+        }
+        return maps;
+    }
+
+    protected override async doCollect(context: KnContextInfo, model: KnModel) : Promise<KnRecordSet> {
+        let rs = await this.doFinding(context,model);
+        return this.createCipherData(context, "collect", this.createRecordSet(rs));
+    }
+
+    protected override async doFind(context: KnContextInfo, model: KnModel) : Promise<KnLabelData[]> {
+        let rs = await this.doFinding(context,model);
+        let labels : KnLabelData[] = [];
+        let maps = this.buildLabelData(rs);
+        for(let [key, value] of maps) {
+            labels.push(value);
+        }
+        return this.createCipherData(context, "find", labels);
+    }
+
+    protected override async doRetrieve(context: KnContextInfo, model: KnModel) : Promise<KnDataEntity> {
+        let rs = await this.doFinding(context,model);
+        let labels : KnDataEntity = {};
+        let maps = this.buildLabelData(rs);
+        for(let [key, value] of maps) {
+            let data : KnDataSet = {};
+            for(let item of value.label) { data[item.name] = item.value };
+            labels[key] = data;
+        }
+        return this.createCipherData(context, "collect", labels);
     }
 
 }
