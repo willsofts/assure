048a5177abc3dc8819be0b99fd6dbc1534a2e121
tassun <32536248+tassun@users.noreply.github.com>
2024-09-25 10:12:51 +0700

    support group and remove by attach no

--- src/handlers/TknAttachHandler.ts
+++ src/handlers/TknAttachHandler.ts
@@ -70,6 +70,27 @@ export class TknAttachHandler extends TknSchemeHandler {
         }
     }
 
+    public async removeAttachNo(attachno: string, model: KnModel = this.model) : Promise<KnRecordSet> {
+        this.logger.debug(this.constructor.name+".removeAttach: no="+attachno);
+        if(!attachno || attachno.trim().length==0) {
+            return Promise.reject(new VerifyError("Attach no is undefined",HTTP.NOT_ACCEPTABLE,-16010));
+        }
+        let db = this.getPrivateConnector(model);
+        try {
+            let sql = new KnSQL("delete from tattachfile ");
+            sql.append("where attachno = ?attachno ");
+            sql.set("attachno",attachno);
+            this.logger.info(this.constructor.name+".removeAttach",sql);
+            let rs = await sql.executeUpdate(db);
+            return this.createRecordSet(rs);
+        } catch(ex: any) {
+            this.logger.error(this.constructor.name,ex);
+            return Promise.reject(this.getDBError(ex));
+        } finally {
+            if(db) db.close();
+        }
+    }
+
     protected async doGet(context: KnContextInfo, model: KnModel) : Promise<KnRecordSet> {
         return this.getAttachInfo(context.params.id as string,context,model);
     }
@@ -90,7 +111,8 @@ export class TknAttachHandler extends TknSchemeHandler {
             let attachid = context.params.id as string;
             let attachtype = context.params.type as string;
             let attachno = context.params.no as string;
-            this.logger.debug(this.constructor.name+".doAttach: id="+attachid+", type="+attachtype+", no="+attachno);
+            let attachgroup = context.params.group as string;
+            this.logger.debug(this.constructor.name+".doAttach: id="+attachid+", type="+attachtype+", no="+attachno+", group="+attachgroup);
             if(!attachid || attachid.trim().length==0) {
                 attachid = context.params.fileid as string;
             }
@@ -107,19 +129,23 @@ export class TknAttachHandler extends TknSchemeHandler {
             sql.append("attachdate = ?attachdate , ");
             sql.append("attachtime = ?attachtime , ");
             sql.append("attachmillis = ?attachmillis ");
-            if(attachtype) {
+            if(attachgroup && attachgroup.trim().length > 0) {
+                sql.append(", attachgroup = ?attachgroup ");
+                sql.set("attachgroup",attachgroup);                
+            }
+            if(attachtype && attachtype.trim().length > 0) {
                 sql.append(", attachtype = ?attachtype ");
                 sql.set("attachtype",attachtype);
             }
-            if(attachno) {
+            if(attachno && attachno.trim().length > 0) {
                 sql.append(", attachno = ?attachno ");
                 sql.set("attachno",attachno);
             }
-            if(attachuser) {
+            if(attachuser && attachuser.trim().length > 0) {
                 sql.append(", attachuser = ?attachuser ");
                 sql.set("attachuser",attachuser);
             }
-            sql.append(", mimetype = ?mimetype, attachgroup = 'FI', attachpath = ?attachpath , ");
+            sql.append(", mimetype = ?mimetype, attachpath = ?attachpath , ");
             sql.append("attachsize = ?attachsize, attachstream = ?attachstream ");
             sql.append("where attachid = ?attachid ");
             sql.set("attachfile",file.filename);
@@ -136,11 +162,12 @@ export class TknAttachHandler extends TknSchemeHandler {
             if(rs.rows.affectedRows==0) {
                 if(!attachno || attachno.trim().length==0) attachno = attachid;
                 if(!attachtype || attachtype.trim().length==0) attachtype = "NONE";
+                if(!attachgroup || attachgroup.trim().length == 0) attachgroup = "FILE";
                 sql.clear();
                 sql.append("insert into tattachfile (attachid,attachno,attachtype,attachfile,sourcefile,attachdate,attachtime,");
                 sql.append("attachmillis,attachuser,mimetype,attachgroup,attachpath,attachsize,attachstream) ");
                 sql.append("values(?attachid,?attachno,?attachtype,?attachfile,?sourcefile,?attachdate,?attachtime,");
-                sql.append("?attachmillis,?attachuser,?mimetype,'FI',?attachpath,?attachsize,?attachstream) ");
+                sql.append("?attachmillis,?attachuser,?mimetype,?attachgroup,?attachpath,?attachsize,?attachstream) ");
                 sql.set("attachid",attachid);
                 sql.set("attachno",attachno);
                 sql.set("attachtype",attachtype);
@@ -151,12 +178,13 @@ export class TknAttachHandler extends TknSchemeHandler {
                 sql.set("attachmillis",now.getTime());
                 sql.set("attachuser",attachuser);
                 sql.set("mimetype",file.mimetype);
+                sql.set("attachgroup",attachgroup);
                 sql.set("attachpath",file.path);
                 sql.set("attachsize",file.size);
                 sql.set("attachstream",stream);
                 rs = await sql.executeUpdate(db,context);
             }
-            rs.rows = { ...rs.rows, attachid: attachid, attachno: attachno, attachtype: attachtype, sourcefile: file.originalname, attachfile: file.filename, attachsize: file.size, mimetype: file.mimetype } ;
+            rs.rows = { ...rs.rows, attachid: attachid, attachno: attachno, attachtype: attachtype, sourcefile: file.originalname, attachfile: file.filename, attachsize: file.size, mimetype: file.mimetype, attachgroup: attachgroup };
             return this.createRecordSet(rs);
         } catch(ex: any) {
             this.logger.error(this.constructor.name,ex);
@@ -167,7 +195,12 @@ export class TknAttachHandler extends TknSchemeHandler {
     }
 
     protected override async doRemove(context: KnContextInfo, model: KnModel) : Promise<KnRecordSet> {
-        return this.removeAttach(context.params.id as string,model);
+        let attachno = context.params.no || context.params.attachno;
+        if(attachno && attachno.trim().length > 0) {
+            return this.removeAttachNo(attachno,model);
+        }
+        let attachid = context.params.id || context.params.attachid;
+        return this.removeAttach(attachid,model);
     }
 
     protected override async doList(context: KnContextInfo, model: KnModel) : Promise<KnRecordSet> {
